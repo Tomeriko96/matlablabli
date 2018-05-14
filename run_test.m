a87629d47373fb09485c4e4fd68f91297d6369c5
Tomer Iwan <IwanTomer@hotmail.com>
2018-05-14 16:31:37 +0200

    niets spannend

--- run_test.m
+++ run_test.m
@@ -4,12 +4,15 @@ close all
 disp('Running the domain/analysis/support model...')
 
 model = l2('test');
-model.simulate(10);
+model.simulate(10, 'domain');
 model.plot();
-% set(gcf,'name','Domain model','numbertitle','off')
-% model.simulate(10, 'analysis');
-% model.plot();
-% set(gcf,'name','Analysis model','numbertitle','off')
+set(gcf,'name','Domain model','numbertitle','off')
+model.simulate(10, 'analysis');
+model.plot();
+set(gcf,'name','Analysis model','numbertitle','off')
+model.simulate(10, 'support');
+model.plot();
+set(gcf,'name','Support Model','numbertitle','off')
 % model.simulate(10, 'support');
 % model.plot();
-% set(gcf,'name','Support model','numbertitle','off')
+% set(gcf,'name','Parameter Adaptation Model','numbertitle','off')
