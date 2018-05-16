7886abe2f3b4e169c580b148e61d95a37cdd7e2a
Tomer Iwan <IwanTomer@hotmail.com>
2018-05-16 15:03:17 +0200

    m

--- run_test.m
+++ run_test.m
@@ -4,15 +4,15 @@ close all
 disp('Running the domain/analysis/support model...')
 
 model = l2('test');
-model.simulate(10, 'domain');
+model.simulate(10);
 model.plot();
-set(gcf,'name','Domain model','numbertitle','off')
-model.simulate(10, 'analysis');
-model.plot();
-set(gcf,'name','Analysis model','numbertitle','off')
-model.simulate(10, 'support');
-model.plot();
-set(gcf,'name','Support Model','numbertitle','off')
+% set(gcf,'name','Domain model','numbertitle','off')
+% model.simulate(10, 'analysis');
+% model.plot();
+% set(gcf,'name','Analysis model','numbertitle','off')
+% model.simulate(10, 'support');
+% model.plot();
+% set(gcf,'name','Support Model','numbertitle','off')
 % model.simulate(10, 'support');
 % model.plot();
 % set(gcf,'name','Parameter Adaptation Model','numbertitle','off')
