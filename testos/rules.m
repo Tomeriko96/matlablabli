4efc6a3c84f90321f8d4e7859bec2cb6dcb64161
Tomer Iwan <IwanTomer@hotmail.com>
2018-05-18 11:55:47 +0200

    sensors + actuators
    sensor: emg
    actuators: buzzer, speaker & servo(ems)
    added to the rules

--- testos/rules.m
+++ testos/rules.m
@@ -8,13 +8,38 @@ end
 
 %ADD RULES BELOW
 
-function result = ddr1( trace, params, t)
-    result = {};
-    a=arduino();
-    value = readVoltage(a, 'A0');
-    result = { result{:} {t+1, 'emg', {value}} };
-end
+% function result = ddr1( trace, params, t)
+%     result = {};
+%     a=arduino();
+%     value = readVoltage(a, 'A0');
+%     result = { result{:} {t+1, 'emg', {value}} };
+% end
 
+% function result = ddr2(trace, params, t)
+%     result={};
+%     a=arduino();
+%     playTone(a,'D5',2400,10);
+% end
+% 
+% function result = ddr3(trace, params, t)
+%     result={};
+%     a=arduino();
+%     
+%     playTone(a,'D3',10,30);
+% end
+
+function result = ddr4(trace, params, t)
+    result={};
+    a=arduino();
+    s = servo(a, 'D4', 'MinPulseDuration', 700*10^-6, 'MaxPulseDuration', 2300*10^-6);
+    for angle = 0:0.2:1
+        writePosition(s, angle);
+        current_pos = readPosition(s);
+        current_pos = current_pos*180;
+        fprintf('Current motor position is %d degrees\n', current_pos);
+        pause(2);
+    end
+end
 % function result = ddr2( trace, params, t)
 %     result = {};
 %     a=arduino();
