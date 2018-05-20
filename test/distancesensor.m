ec977d3e454746bc2b0aec5c3e26f9a71e39b460
Tomer Iwan <IwanTomer@hotmail.com>
2018-05-20 13:04:46 +0200

    useless

--- test/distancesensor.m
+++
@@ -1,4 +0,0 @@
-function result = jump_height(controller)
-    pin = 'A5';
-    result = predicate('jump_height', readVoltage(controller, pin));
-end
--- test/emg.m
+++
@@ -1,4 +0,0 @@
-function result = emg(controller)
-    pin = 'A0';
-    result = predicate('emg', readVoltage(controller, pin));
-end
